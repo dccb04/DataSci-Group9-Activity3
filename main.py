589724006df5f2c62c86da51878e30ff0178c133
Andrei Turgo <abturgo@gmail.com>
2024-10-04 15:51:58 +0800

    Updating the df images

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import altair as alt
-from io import StringIO
+from PIL import Image
 
 #git add .
 #git commit  -m ""
@@ -23,26 +23,18 @@ The fraudulent URL Detection dataset classifies URLs as legitimate or fraudulent
 st.write("Dataset: [Phishing URL Detection](https://www.kaggle.com/datasets/sergioagudelo/phishing-url-detection)")
 
 ##############################################################################################################################################################################################
-
-#To read the csv file
-df = pd.read_csv("datasets/out.csv")
-
-##############################################################################################################################################################################################
-buffer = StringIO()
-df.info(buf=buffer)
-df_info_as_string = buffer.getvalue()
-st.write("## Shows the relevant datatypes in the dataset")
-st.text(df_info_as_string) # Show the relevant information in the dataset such as Data Types
+imageInfo = Image.open('assets/info.png')
+st.image(imageInfo)
 
 ##############################################################################################################################################################################################
 #Show the null values of each data types.
-st.write("## Show the null values.")
-st.write(df.isna().sum()) 
+imageNull = Image.open('assets/nullValues.png')
+st.image(imageNull)
 
 ##############################################################################################################################################################################################
 #This will generate Descriptive Statistics
-st.write("## Generate descriptive Statistics")
-st.write(df.describe()) 
+describe = Image.open('assets/describe.png')
+st.image(describe)
 
 ##############################################################################################################################################################################################
 
@@ -78,30 +70,8 @@ st.write("""
 
 st.write("## 1. Legitimacy of a URL that starts with an IP.")
 
-#To know the values when combining label and starts_with_ip data types before creating the Visual.
-urlLegitimacy = df.groupby(['starts_with_ip', 'label']).size().unstack()
-urlLegitimacy
-
-#First graph
-
-def bar_plot_legitimacy_basedOn_IP():
-  plt.figure(figsize=(7, 7)) # define the width and height of the graph
-
-  ax = sns.countplot(x='label', hue='starts_with_ip', data=df)
-  plt.title('URL that starts with IP add. Is it Phishing or not?')
-
-  plt.xlabel('Legitimacy of URLs (Legitimate or Phishing)')  # label for X-Axis
-  plt.ylabel('Totalility of URLs')       # label for Y-Axis
-
-
-  # You can use this to show the actual count of each bars
-  for p in ax.patches:
-    ax.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
-
-  st.pyplot(plt)
-  plt.clf()
-
-bar_plot_legitimacy_basedOn_IP()
+image1 = Image.open('assets/FirstGraph.png')
+st.image(image1)
 
 st.write("## Observations:")
 st.write("""
@@ -114,28 +84,8 @@ st.write("""
 
 st.write("## 2. The Source of the URL of each and every phishing sites.")
 
-# With the use of 'source and 'label' datatypes. We can know and picturize first what will be the graph be consist of.
-urlLegitimacy = df.groupby(['source', 'label']).size().unstack() 
-urlLegitimacy
-
-def pie_chart_phishing_distribution():
-
-    phishing_df = df[df['label'] == 'phishing'] # Filtering  the Data to include only which are considered as Phishing URLs
-
-    # Count occurrences of each source in the filtered DataFrame
-    source = phishing_df['source'].value_counts()
-
-    # Setting the Color of each pie.
-    colors = ['salmon', 'green', 'yellow'][:len(source)]
-
-    # Create the pie chart with increased .00 decimal places
-    plt.pie(source, labels=source.index, autopct='%.2f%%', colors=colors)
-    plt.title('Source Distribution of Phishing URLs')
-    
-    st.pyplot(plt)
-    plt.clf()
-
-pie_chart_phishing_distribution()
+image2 = Image.open('assets/Second Graph.png')
+st.image(image2)
 
 st.write("## Observations on Phishing Sources:")
 st.write("""
